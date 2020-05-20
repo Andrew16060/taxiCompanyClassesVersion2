9a260d75aeaa38ab4e23f12abf66e747fd842333
Andrew Booth <16060@bayfield-high.school.nz>
2020-05-21 09:04:57 +1200

    Add project files.

---
+++ taxiCompanyClasses/Form1.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace taxiCompanyClasses
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            taxiList.Add(new taxi("Dunedin Taxi", 4543333, 3, 2.55));
+            taxiList.Add(new taxi("United Taxi", 4662222, 2.90, 3));
+            taxiList.Add(new taxi("Southern Taxi", 5005000, 2.50, 2.60));
+        }
+
+        List<taxi> taxiList = new List<taxi>();
+
+        private void btnCalculate_Click(object sender, EventArgs e)
+        {
+            for (int i = 0; i < taxiList.Count; i++)
+            {
+                lboxOutput.Items.Add(taxiList[i].CalculateCost(Convert.ToDouble(txtDistance.Text)));
+            }
+        }
+    }
+}
---
+++ taxiCompanyClasses/taxi.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace taxiCompanyClasses
+{
+    class taxi
+    {
+        private string name;
+        private int phoneNumber;
+        private double pickupFee;
+        private double ratePerKM;
+
+        public taxi (string argsName, int argsPhoneNumber, double argsPickupFee, double argsRatePerKM)
+        {
+            name = argsName;
+            phoneNumber = argsPhoneNumber;
+            pickupFee = argsPickupFee;
+            ratePerKM = argsRatePerKM;
+        }
+
+        public double CalculateCost(double Distance)
+        {
+            double cost = pickupFee + ratePerKM * Distance;
+            return cost;
+        }
+    }
+}
